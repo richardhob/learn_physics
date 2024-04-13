4ef372abe4a7f70bb438ff039d747a30e453dca8
Ricky <richardhob@gmail.com>
2024-04-13 07:03:08 -0700

    Add simple graphing example, readme, and make plan to document / display things ...

---
+++ src/graphing/simple_xy.hs
@@ -0,0 +1,24 @@
+
+module Main where
+
+import Graphics.Gnuplot.Simple
+
+type R = Double
+
+tRange :: [R]
+tRange = [0,0.01..5]
+
+yPos :: R -> R -> R -> R -> R
+yPos y0 vy0 ay t = y0 + vy0 * t + ay * t**2 / 2
+
+plot1 ::  IO ()
+plot1 = let details = [Title  "Projectile Motion"
+                      ,XLabel "Time (s)"
+                      ,YLabel "Projectile Height (m)"
+                      ,PNG    "projectile.png"
+                      ,Key    Nothing
+                      ] 
+        in plotFunc details tRange (yPos 0 20 (-9.8))
+
+main :: IO ()
+main = plot1
