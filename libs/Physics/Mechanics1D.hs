8b079a480e3eca1e9c53ebca8e14d0c39f41010c
Ricky <richardhob@gmail.com>
2024-04-25 18:28:35 -0700

    Resolve build errors

--- libs/Physics/Mechanics1D.hs
+++ libs/Physics/Mechanics1D.hs
@@ -23,7 +23,7 @@ newtonSecond1D mass forces (t0, x0, v0)
 
 euler1D :: TimeStep -> (State1D -> (R,R,R)) -> State1D -> State1D
 euler1D dt f (t0, x0, v0)
-    = let (_, _, dvdt) = f (t0, x0 v0)
+    = let (_, _, dvdt) = f (t0, x0, v0)
           t1 = t0 + dt
           x1 = x0 + v0 * dt
           v1 = v0 + dvdt * dt
@@ -37,24 +37,24 @@ statesTXV dt mass initial forces = iterate (updateTXV dt mass forces) initial
 
 velocity1D :: [State1D] -> Time -> Velocity
 velocity1D states t
-    = let (t0,_,_) = take 1 states
-          (t1,_,_) = take 1 $ drop 1 states
+    = let (t0,_,_) = head $ take 1 states
+          (t1,_,_) = head $ take 1 $ drop 1 states
           dt = t1 - t0
           steps = abs $ round (t / dt)
-          (_,_,vn) = take 1 $ drop steps states
+          (_,_,vn) = head $ take 1 $ drop steps states
       in vn
 
-velocityFTXV :: TimeStep -> State1D -> [State1D -> Force] -> Time -> Velocity
+velocityFTXV :: TimeStep -> Mass -> State1D -> [State1D -> Force] -> Time -> Velocity
 velocityFTXV dt mass txv0 forces = velocity1D (statesTXV dt mass txv0 forces)
 
 position1D :: [State1D] -> Time -> Position
 position1D states t
-    = let (t0,_,_) = take 1 states
-          (t1,_,_) = take 1 $ drop 1 states
+    = let (t0,_,_) = head $ take 1 states
+          (t1,_,_) = head $ take 1 $ drop 1 states
           dt = t1 - t0
           steps = abs $ round (t / dt)
-          (_,xn,_) = take 1 $ drop steps states
+          (_,xn,_) = head $ take 1 $ drop steps states
       in xn
 
-positionFTXV :: TimeStep -> State1D -> [State1D -> Force] -> Time -> Position
+positionFTXV :: TimeStep -> Mass -> State1D -> [State1D -> Force] -> Time -> Position
 positionFTXV dt mass txv0 forces = position1D (statesTXV dt mass txv0 forces)
