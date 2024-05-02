04fe8420b872680feee036f37e35578895cae4f2
Ricky <richardhob@gmail.com>
2024-05-01 20:21:39 -0700

    Finish 15_6

--- src/ch15/exercises.hs
+++ src/ch15/exercises.hs
@@ -4,6 +4,7 @@ module Main where
 import Graphics.Gnuplot.Simple
 
 import Physics.Mechanics1D
+import Physics.Newton2 (fAir)
 
 -- Chapter 15 Exercises
 
@@ -88,13 +89,62 @@ plot_15_5
 -- Let's drop a ping pong ball and a bowling ball from 100m and 500m. Make
 -- graphs of velocity vs time, as well as velocity vs. vertical position.
 -- 
--- Use C = 1/2 ? and make reasonable assumptions about the bowling ball and ping
--- pong ball
+-- Use a drag coefficient C = 1/2 and make reasonable assumptions about the
+-- bowling ball and ping pong ball
 plot_15_6 :: IO () 
-plot_15_6 = do
+plot_15_6 
+    = let dt = 0.1
+          h1 = 100 -- m
+          -- h2 = 500 -- m
+          c = 0.5
+          rho = 1.225
+
+          -- Bowling ball -> (10 kg, 6 * 0.25 m*m)
+          -- Ping Pong ball -> (0.1 kg, 0.10 m*m)
+          _get height mass area
+              = let gravity (_, _, _) = -9.8 * mass
+                    f_air (_, _, v) = fAir c rho area v
+                    forces = [gravity, f_air]
+                    states = statesTXV dt mass (0, height, 0) forces
+                    s = takeWhile (\(_, x, _) -> x > 0) states
+                in s
+
+          bb_area = 1.5 -- m^2
+          bb_mass = 10 -- kg
+
+          pp_area = 0.01 -- m^2
+          pp_mass = 0.01 -- kg
+
+          bb_style = defaultStyle {lineSpec=CustomStyle [LineTitle "Bowling ball"]}
+          pp_style = defaultStyle {lineSpec=CustomStyle [LineTitle "Ping Pong ball"]}
+
+          bb_100 = _get h1 bb_mass bb_area
+          bb_100_v = map (\(t, _, v) -> (t, v)) bb_100
+          bb_100_p = map (\(_, x, v) -> (x, v)) bb_100
+          pp_100 = _get h1 pp_mass pp_area
+          pp_100_v = map (\(t, _, v) -> (t, v)) pp_100
+          pp_100_p = map (\(_, x, v) -> (x, v)) pp_100
+
+          details_100_v = [Title "Bowling Ball vs. Ping Pong Ball Velocity vs Time"
+                        ,XLabel "Time (s)"
+                        ,YLabel "Velocity (m/s)"
+                        ,PNG "notes/images/ch15_ex_15_5_100_v.png"
+                        ]
+
+          details_100_p = [Title "Bowling Ball vs. Ping Pong Ball Velocity vs. Position"
+                        ,XLabel "Position (m)"
+                        ,YLabel "Velocity (m/s)"
+                        ,PNG "notes/images/ch15_ex_15_5_100_p.png"
+                        ]
+
+      in do
+             plotPathsStyle details_100_v [(bb_style, bb_100_v), (pp_style, pp_100_v)]
+             plotPathsStyle details_100_p [(bb_style, bb_100_p), (pp_style, pp_100_p)]
+           
 
 main :: IO () 
 main = do
     plot_15_1
     plot_15_3
     plot_15_5
+    plot_15_6
